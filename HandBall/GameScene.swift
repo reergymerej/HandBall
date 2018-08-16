45c7ad948bbf216ec300746be601964ab4e78fe7
Jeremy Greer <jex.grizzle@gmail.com>
2018-08-15 21:29:34 -0400

    Move paddle, bounce ball

--- HandBall/GameScene.swift
+++ HandBall/GameScene.swift
@@ -10,7 +10,8 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+   
+    private var isTouchingPaddle = false
 
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor.black
@@ -21,12 +22,16 @@ class GameScene: SKScene {
     
     func addPaddle() {
         let paddle = SKShapeNode(rect: CGRect(x: -100, y: -25, width: 200, height: 50))
+        paddle.name = "paddle"
         paddle.fillColor = SKColor.darkGray
         paddle.strokeColor = SKColor.black
         paddle.position = CGPoint(x: frame.midX, y: frame.midY - 100)
         
         paddle.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 200, height: 50))
-        paddle.physicsBody?.affectedByGravity = false
+        // paddle.physicsBody?.affectedByGravity = false
+        // paddle.physicsBody?.allowsRotation = false
+        // paddle.physicsBody?.restitution = 1.0
+        paddle.physicsBody?.isDynamic = false
         addChild(paddle)
     }
     
@@ -40,8 +45,8 @@ class GameScene: SKScene {
         ball.physicsBody = SKPhysicsBody(circleOfRadius: ballSize)
         ball.physicsBody?.allowsRotation = false
         ball.physicsBody?.restitution = 1.0
-        ball.physicsBody?.affectedByGravity = false
-        ball.physicsBody?.friction = 0.0
+        ball.physicsBody?.affectedByGravity = true
+        // ball.physicsBody?.friction = 0.0
         addChild(ball)
     }
     
@@ -56,19 +61,35 @@ class GameScene: SKScene {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if let body = getTouchedBody(touch) {
+                if body.node?.name == "paddle" {
+                    self.isTouchingPaddle = true
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            if self.isTouchingPaddle {
+                let paddle = self.childNode(withName: "paddle")
+                let point = getPointFromTouch(touch)
+                paddle?.position.x = point.x
+                paddle?.position.y = min(point.y, frame.height / 3)
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            if let body = getTouchedBody(touch) {
-                let shove = CGVector(dx: 23.0, dy: 100.0)
-                let point = getPointFromTouch(touch)
-                body.applyImpulse(shove, at: point)
-            }
-        }
+        // if let touch = touches.first {
+        //     if let body = getTouchedBody(touch) {
+        //         let shove = CGVector(dx: 23.0, dy: 100.0)
+        //         let point = getPointFromTouch(touch)
+        //         body.applyImpulse(shove, at: point)
+        //     }
+        // }
+        self.isTouchingPaddle = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # HandBall
 
 I want to learn about handling physics in Swift, so here goes.
+
+## Notes
+
+I've got two bodies that I can apply impulses to and a body around the view to
+keep everything in place.  I want to learn how to drag a body around now, so I
+can use it knock into the others.
+
+According to this,
+https://www.raywenderlich.com/2598-how-to-create-a-breakout-game-using-spritekit,
+the way to do this is to:
+
+1. Update a state value when a touch is made on the paddle.
+2. Add code in the `touchesMoved` hook to reposition the paddle if needed.
